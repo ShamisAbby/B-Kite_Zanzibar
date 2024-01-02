925c2564de93f57543126a5028deb16f61c4e9b5
ShamisAbby <shamisaziz52@gmail.com>
2024-01-02 18:38:05 +0300

    Delete some pages 2-Jan-24

---
+++ backend/desktop-navigation.php
@@ -0,0 +1,79 @@
+<div id="mySidenav" class="sidenav">
+    <script language="JavaScript">
+        function changepic(iobj_id, bild) { //alert(document.getElementById(obj_id).src);
+            document.getElementById(obj_id).src = '//assets/demandit/modul_12/' + bild;
+        }
+
+        function changeback(obj_id, bild) {
+            //alert(document.getElementById(obj_id).src);
+            document.getElementById(obj_id).style.background = 'url(//assets/demandit/modul_12/' + bild + ')';
+        }
+    </script>
+    <ul class="fw-nav-level2">
+        <li class="fw-nav-btn fw-nav-selected fw-nav-first" id="navbutt_20844_56535"><a class="fw-nav-selected fw-nav-first" href="./index.php" id="navlink_20844_56535">Home</a></li>
+        <li class="fw-nav-btn fw-nav-selected fw-nav-first" id="navbutt_20844_56535"><a class="fw-nav-btn fw-nav-first" href="./about-us.php" id="navlink_20844_56535">About us</a></li>
+        <li class="fw-nav-btn fw-nav-selected fw-nav-first" id="navbutt_20844_56535"><a class="fw-nav-btn fw-nav-first" href="./the-spots.php" id="navlink_20844_56535">Spots</a></li>
+        <li class="fw-nav-btn fw-nav-selected fw-nav-first" id="navbutt_20844_56535"><a class="fw-nav-btn fw-nav-first" href="./bkite-pricing.php" id="navlink_20844_56535">Price List</a></li>
+        <li class="fw-nav-btn fw-nav-selected fw-nav-first" id="navbutt_20844_56535"><a class="fw-nav-btn fw-nav-first" href="./contact.php" id="navlink_20844_56535">Contact us</a></li>
+        <li class="fw-nav-btn fw-nav-selected fw-nav-first" id="navbutt_20844_56535"><a class="fw-nav-btn fw-nav-first" href="https://www.instagram.com/bkite_zanzibar/" id="navlink_20844_56535">Instagram</a></li>
+        <li class="fw-nav-btn fw-nav-selected fw-nav-first" id="navbutt_20844_56535"><a class="fw-nav-btn fw-nav-first" href="https://web.facebook.com/profile.php?id=100088668205495" id="navlink_20844_56535">Facebook</a></li>
+        <li class="fw-nav-btn fw-nav-selected fw-nav-first" id="navbutt_20844_56535"><a class="fw-nav-btn fw-nav-first" href="https://www.tripadvisor.com/Attraction_Review-g616020-d23455194-Reviews-Bkite_Zanzibar_Kitesurfing_School-Paje_Zanzibar_Island_Zanzibar_Archipelago.php" id="navlink_20844_56535">Tripadvisor</a></li>
+
+        <li class="fw-nav-btn" id="navbutt_46816_56535"><a href="#" id="navlink_46816_56535">Language</a>
+            <ul class="fw-nav-level3">
+                <li class="fw-nav-btn fw-nav-first" id="navbutt_46830_56535"><a class="fw-nav-first" href="#" id="navlink_46830_56535">English</a></li>
+                <li class="fw-nav-btn fw-nav-last" id="navbutt_46832_56535"><a class="fw-nav-last" href="#" id="navlink_46832_56535">France</a></li>
+            </ul>
+        </li>
+
+
+        <!-- <li class="fw-nav-btn" id="navbutt_46814_56535"><a href="index190d.php?page=137015&amp;cfid=149103729&amp;cftoken=904b09e5e11cbad1-3E911D37-B04E-2DC0-DA1889DD4515BC68" id="navlink_46814_56535">Rates & Location</a>
+            <ul class="fw-nav-level3">
+                <li class="fw-nav-btn fw-nav-first" id="navbutt_46825_56535"><a class="fw-nav-first" href="#" id="navlink_46825_56535">Rates</a></li>
+                <li class="fw-nav-btn" id="navbutt_46828_56535"><a href="#" id="navlink_46828_56535">Rooms</a>
+                </li>
+                <li class="fw-nav-btn" id="navbutt_46826_56535"><a href="#" id="navlink_46826_56535">Surf
+                        Guiding</a></li>
+                <li class="fw-nav-btn" id="navbutt_46827_56535"><a href="#" id="navlink_46827_56535">Surf
+                        Equipment</a></li>
+                <li class="fw-nav-btn" id="navbutt_46829_56535"><a href="#" id="navlink_46829_56535">Point break
+                        day</a></li>
+                <li class="fw-nav-btn fw-nav-last" id="navbutt_48404_56535"><a class="fw-nav-last" href="#" id="navlink_48404_56535">7 Day Fun</a></li>
+            </ul>
+        </li> -->
+
+        <!-- <li class="fw-nav-btn" id="navbutt_46817_56535"><a href="index34a5.php?page=137017&amp;cfid=149103729&amp;cftoken=904b09e5e11cbad1-3E911D37-B04E-2DC0-DA1889DD4515BC68" id="navlink_46817_56535">Gallery</a>
+            <ul class="fw-nav-level3">
+                <li class="fw-nav-btn fw-nav-first" id="navbutt_46833_56535"><a class="fw-nav-first" href="#" target="_blank" id="navlink_46833_56535">Instagram</a></li>
+                <li class="fw-nav-btn" id="navbutt_46835_56535"><a href="#" id="navlink_46835_56535">Camp
+                        Videos</a></li>
+                <li class="fw-nav-btn fw-nav-last" id="navbutt_46834_56535"><a class="fw-nav-last" href="#" id="navlink_46834_56535">Surf Videos</a></li>
+            </ul>
+        </li>
+        <li class="fw-nav-btn" id="navbutt_46818_56535"><a href="indexc3b2.php?page=137018&amp;cfid=149103729&amp;cftoken=904b09e5e11cbad1-3E911D37-B04E-2DC0-DA1889DD4515BC68" id="navlink_46818_56535"><br>About</a>
+            <ul class="fw-nav-level3">
+                <li class="fw-nav-btn fw-nav-first" id="navbutt_46836_56535"><a class="fw-nav-first" href="team.php" id="navlink_46836_56535">Team</a></li>
+                <li class="fw-nav-btn fw-nav-last" id="navbutt_46837_56535"><a class="fw-nav-last" href="surfing.php" id="navlink_46837_56535">Surfing</a></li>
+            </ul>
+        </li>
+        <li class="fw-nav-btn fw-nav-last" id="navbutt_46840_56535">
+            <a class="fw-nav-last" href="index252b.php?page=137035&amp;cfid=149103729&amp;cftoken=904b09e5e11cbad1-3E911D37-B04E-2DC0-DA1889DD4515BC68" id="navlink_46840_56535">Contact & Booking
+            </a>
+            <ul class="fw-nav-level3">
+                <li class="fw-nav-btn fw-nav-first" id="navbutt_46841_56535">
+                    <a class="fw-nav-first" href="#" id="navlink_46841_56535">Contact
+                    </a>
+                </li>
+
+                <li class="fw-nav-btn" id="navbutt_46819_56535">
+                    <a href="request.php" id="navlink_46819_56535">Request
+                    </a>
+                </li>
+                <li class="fw-nav-btn fw-nav-last" id="navbutt_46842_56535">
+                    <a class="fw-nav-last" href="booking.php" id="navlink_46842_56535">Booking
+                    </a>
+                </li>
+            </ul>
+        </li> -->
+    </ul>
+</div>
